85955cc42f2bab568cf20323a2659cfc8b826336
Barbara Hannigan <sba23118@student.cct.ie>
2025-05-13 19:03:20 +0100

    Update Uber_pickups.py

--- Uber_pickups.py
+++ Uber_pickups.py
@@ -20,3 +20,11 @@ def load_data(nrows):
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
