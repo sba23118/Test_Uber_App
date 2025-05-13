8b63dacea09378b7d4e0dde1f4a234f662f93cb0
Barbara Hannigan <sba23118@student.cct.ie>
2025-05-13 18:57:33 +0100

    Update Uber_pickups.py

--- Uber_pickups.py
+++ Uber_pickups.py
@@ -12,7 +12,7 @@ def load_data(nrows):
 	data=pd.read_csv(DATA_URL, nrows=nrows)
 	lowercase=lambda x: str(x).tolower()
 	data.rename(lowercase, axis="columns", inplace=True)
-	data[DATE_COLUMN=pd.to_datetime(data[DATE_COLUMN])
+	data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
 	return data
 
 data_load_state=st.text("Loading Data.....")
